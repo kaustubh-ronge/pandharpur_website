7ab97a2257b4afc2711927e520b6d314acc12f0c
kaustubh-ronge <kaustubhronge25@gmail.com>
2025-09-11 09:07:29 +0530

    11-09-2025

--- data/HeaderData/headerData.js
+++ data/HeaderData/headerData.js
@@ -9,7 +9,7 @@ export const navItems = [
   { name: "Pandharpur Attractions", href: "/other-attractions", icon: <MapPin className="h-5 w-5" /> },
   { name: "Pandharpur Festivals", href: "/festivals", icon: <CalendarDays className="h-5 w-5" /> },
   { name: "Yatra&Darshan Guide", href: "/guide", icon: <BookOpen className="h-5 w-5" /> },
-  { name: "Booking", href: "/other-information", icon: <NotebookPen className="h-5 w-5" /> }
+  { name: "Booking", href: "/information-page", icon: <NotebookPen className="h-5 w-5" /> }
 ];
 
 export const quickLinks = [
